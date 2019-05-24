caf0f107ccd92c64ed8b6d1f98734d20e5ec011c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2019-05-24 14:57:47 +0200

    Add fonts locally

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import 'react-hot-loader/patch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'typeface-lato';
+import 'typeface-roboto-mono';
 
 ReactDOM.render(<App />, document.getElementById('root'));
