516bfd7e6d04c3663a50b71986ccf5c1efba647e
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2019-05-31 23:47:33 +0200

    Add indicator when offline (#52)

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,23 +1,26 @@
 import React, { FunctionComponent, useEffect } from 'react';
 import { StyledMain } from './StyledMain';
 import { connect, MapDispatchToProps } from 'react-redux';
-import { connectProvider } from '../../store/network';
+import { connectProvider, setNetwork } from '../../store/network';
 import { providers } from 'ethers';
+import { NETWORK_OFFLINE } from '../../config';
 
 interface Web3 {
   currentProvider: any;
 }
 
-declare const window: { web3?: Web3 };
+declare const window: { web3?: Web3 } & Window;
 
 interface DispatchProps {
   handleConnect(provider: providers.Provider): void;
+
+  handleOffline(): void;
 }
 
 type Props = DispatchProps;
 
-const Main: FunctionComponent<Props> = ({ handleConnect, children }) => {
-  useEffect(() => {
+const Main: FunctionComponent<Props> = ({ handleConnect, handleOffline, children }) => {
+  const initialize = () => {
     const provider =
       window.web3 && window.web3.currentProvider
         ? new providers.Web3Provider(window.web3.currentProvider)
@@ -28,15 +31,34 @@ const Main: FunctionComponent<Props> = ({ handleConnect, children }) => {
           ]);
 
     handleConnect(provider);
+  };
+
+  useEffect(() => {
+    if (navigator.onLine) {
+      initialize();
+    }
+
+    window.addEventListener('online', initialize);
+
+    return () => window.removeEventListener('online', initialize);
+  });
+
+  useEffect(() => {
+    if (!navigator.onLine) {
+      handleOffline();
+    }
+
+    window.addEventListener('offline', handleOffline);
+
+    return () => window.removeEventListener('offline', handleOffline);
   });
 
   return <StyledMain>{children}</StyledMain>;
 };
 
 const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = dispatch => ({
-  handleConnect(provider: providers.Provider): void {
-    dispatch(connectProvider(provider));
-  }
+  handleConnect: (provider: providers.Provider) => dispatch(connectProvider(provider)),
+  handleOffline: () => dispatch(setNetwork(NETWORK_OFFLINE))
 });
 
 export default connect(
