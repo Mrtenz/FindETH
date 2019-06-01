1077fd2c166f8c26bd7a9b52bac30550db74a353
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2019-06-01 21:43:34 +0200

    Setup enzyme-to-json

--- src/components/ui/Address/Address.test.tsx
+++ src/components/ui/Address/Address.test.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Address from './Address';
 
-it('renders an address', () => {
-  const wrapper = shallow(<Address address="0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520" />);
+it('renders a snapshot', () => {
+  const component = shallow(<Address address="0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520" />);
 
-  expect(wrapper.prop('truncate')).toBeFalsy();
-  expect(wrapper.contains('0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520')).toBe(true);
+  expect(component).toMatchSnapshot();
+});
+
+it('renders an address', () => {
+  const component = shallow(<Address address="0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520" />);
+
+  expect(component.prop('truncate')).toBeFalsy();
+  expect(component.contains('0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520')).toBe(true);
 });
 
 it('renders a truncated address', () => {
-  const wrapper = shallow(
+  const component = shallow(
     <Address address="0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520" truncate={true} />
   );
 
-  expect(wrapper.contains('0x4bbe...1520')).toBe(true);
+  expect(component.contains('0x4bbe...1520')).toBe(true);
 });
