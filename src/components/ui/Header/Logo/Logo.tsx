2c5cc7d9145c6de1aff2f8a0c556402fa3204e8e
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2019-06-07 10:53:41 +0200

    Reorganize images

--- src/components/ui/Header/Logo/Logo.tsx
+++ src/components/ui/Header/Logo/Logo.tsx
@@ -1,6 +1,6 @@
 import React, { DetailedHTMLProps, FunctionComponent, ImgHTMLAttributes } from 'react';
 import { LogoImage } from './StyledLogo';
-import findEthLogo from '../../../../assets/images/logos/findeth.svg';
+import findEthLogo from '../../../../assets/images/logos/findeth/findeth.svg';
 
 type Props = Omit<DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>, 'ref'>;
 
