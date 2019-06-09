b43e802100eb9692906ced5cfd42849f9872fd3e
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2019-06-09 11:44:15 +0200

    Reduce opacity on button text when disabled

--- src/components/ui/Button/Button.ts
+++ src/components/ui/Button/Button.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Button as UIButton } from '@mycrypto/ui';
+import { transparentize } from 'polished';
 
 interface Props {
   disabled?: boolean;
@@ -9,6 +10,14 @@ const Button = styled(UIButton)<Props>`
   margin-top: 18px;
   background: ${({ disabled, theme }) => disabled && theme.buttonDisabledBackground};
   font-size: 1rem;
+  color: ${({ theme, disabled, secondary }) =>
+    secondary
+      ? disabled
+        ? transparentize(0.25, theme.primary)
+        : theme.primary
+      : disabled
+      ? transparentize(0.25, 'white')
+      : 'white'};
 
   &:hover {
     background: ${({ disabled, theme }) => disabled && theme.buttonDisabledBackground};
